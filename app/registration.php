7dba17cfa6d3c677127d1f42854c03209457614b
/poldev <johnangeloaustin013@gmail.com>
2025-10-12 20:14:50 +0800

    das

--- app/registration.php
+++ app/registration.php
@@ -11,7 +11,7 @@ if (isset($_POST['signup'])) {
 	$full_name = $fname.' '.$lname;
 
 				if (($password == $password2)) {
-					$hash = password_hash($pass, PASSWORD_DEFAULT);
+					$hash = password_hash($password, PASSWORD_DEFAULT);
 					$sql = "INSERT INTO `users`( `fname`, `mname`, `lname`, `suffix`, `full_name`, `username`, `password`, `role`) VALUES ('$fname', '$lname','$mname', '$suffix', '$full_name', '$username', '$hash', 'user')";
 					$result = mysqli_query($sqli, $sql);
 					if ($result) {
